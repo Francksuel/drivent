ae7a0b14cba4d3b7e55ab8e6d2366db2d72d345b
Francksuel <francksuelbarbosa@gmail.com>
2022-11-14 10:23:00 -0300

    fix: remove unused type

--- src/protocols.ts
+++ src/protocols.ts
@@ -11,14 +11,6 @@ export type CEP = {
   uf: string,
 };
 
-export type ViaCEPAddress = {
-  logradouro: string,
-  complemento: string,
-  bairro: string,
-  localidade: string,
-  uf: string,
-};
-
 export type RequestError = {
   status: number,
   data: object | null,
